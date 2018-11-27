dc7252d4ba890ef1f89e3dcf6da2a0a8043dc2f0
Susan Ottwell <sottwell@sottwell.com>
2018-11-27 23:30:01 +0200

    Update install.php
    update links for Revo 2.7.0

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
-$version = '1.6.19';
+$version = '1.6.20';
 
 error_reporting(0);
 ini_set('display_errors', 0);
@@ -22,6 +22,25 @@ if (ini_get('allow_url_fopen')) {
 }
 
 $InstallData = array(
+	
+    'revo2.7.0-pl'      => array(
+        'tree'     => 'Revolution',
+        'name'     => 'MODX Revolution 2.7.0 Traditional (11.27.2018)',
+        'link'     => 'https://modx.com/download/direct/modx-2.7.0-pl.zip',
+        'location' => 'setup/index.php'
+    ),
+    'revo2.7.0-ad'      => array(
+        'tree'     => 'Revolution',
+        'name'     => 'MODX Revolution 2.7.0 Advanced (11.27.2018)',
+        'link'     => 'https://modx.com/download/direct/modx-2.7.0-pl-advanced.zip',
+        'location' => 'setup/index.php'
+    ),
+    'revo2.7.0-sdk'      => array(
+        'tree'     => 'Revolution',
+        'name'     => 'MODX Revolution 2.7.0 SDK (11.27.2018)',
+        'link'     => 'https://modx.com/download/direct/modx-2.7.0-pl-sdk.zip',
+        'location' => 'setup/index.php'
+    ),
 	
    'revo2.6.5-pl'      => array(
         'tree'     => 'Revolution',
